866d60e2d7d12acc9d47a72c7f92d7afc7768b9b
chucky <charles.ssekitto@cobblestoneenergy.com>
2021-08-15 12:30:26 +0100

    clean up some of the dead code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import "./App.css";
-import Book from "./components/BooksContainer";
+import Books from "./components/BooksContainer";
 
 function App() {
   return (
-    <div className="">
+    <div className="container">
       <header className="">
-        <h3>My Reads App </h3>
+        <h1 id="app-header">My Reads App </h1>
       </header>
-
       <main>
         <div className="main">
-          <div>currently Reading</div>
-          <Book />
-          <div>Want to read</div>
-          <div>Read</div>
+          <Books />
         </div>
       </main>
-
-      {/* TODO  Work on the app header*/}
-      {/* [X]  Add a few app styles to render the components*/}
-      {/* TODO  Work on the component that renders the books*/}
-      {/* TODO  Work on splitting the displayed books into currently reading,want to read, read*/}
-      {/* TODO  Work on the moving of the books into the different categories listed above, this should enable a user to move the different books into categories of currently reading,want to read, read*/}
       {/* TODO  Work on the search functionality of the application*/}
     </div>
   );
