af55340ef3db4154ba874f79b73f7cd9494051c5
Ssekitto charlse <bozicslxye1@gmail.com>
2021-08-15 04:36:32 -0700

    add the components to render books from the api (#1)
    * add the components to render books from the api
    
    * add categorizing of books
    
    * #3 add app styles to make app presentable
    
    * clean up some of the dead code
    
    * #5,#4 split the app data into shelves
    
    Co-authored-by: chucky <charles.ssekitto@cobblestoneenergy.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import Books from "./components/BooksContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Welcome to the book case app </h3>
+    <div className="container">
+      <header className="">
+        <h1 id="app-header">My Reads App </h1>
       </header>
+      <main>
+        <div className="main">
+          <Books />
+        </div>
+      </main>
+      {/* TODO  Work on the search functionality of the application*/}
     </div>
   );
 }
