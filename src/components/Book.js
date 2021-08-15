0069d7e1fee0a978ecddff5cc7a9a6685dc5ccf7
Ssekitto charlse <bozicslxye1@gmail.com>
2021-08-15 04:41:33 -0700

    Ft add books (#7)
    * add the components to render books from the api
    
    * add categorizing of books
    
    * #3 add app styles to make app presentable
    
    * clean up some of the dead code
    
    * #5,#4 split the app data into shelves
    
    Co-authored-by: chucky

---
+++ src/components/Book.js
@@ -0,0 +1,30 @@
+const Book = (props) => {
+  return (
+    <div className="book">
+      <div className="book-image">
+        <img src={props.image} alt="" />
+      </div>
+      <p className="book-title">{props.title}.</p>
+      <p className="book-author">By {props.author}</p>
+
+      <div className="selection">
+        <label htmlFor="categories">select books to</label>:
+        <select
+          name="categories"
+          id="book-categories"
+          onChange={props.handleDropdownChange}
+        >
+          <option>Move</option>
+          <option value={`None,${props.title}`}>None</option>
+          <option value={`read,${props.title}`}>read</option>
+          <option value={`wishlist,${props.title}`}>want to read</option>
+          <option value={`currentlyReading,${props.title}`}>
+            currently reading
+          </option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
