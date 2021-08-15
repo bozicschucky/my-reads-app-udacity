70ca58367638e991c0cd160a2991af418c072cb5
Ssekitto charlse <bozicslxye1@gmail.com>
2021-08-15 04:39:31 -0700

    Revert "add the components to render books from the api (#1)" (#6)
    This reverts commit af55340ef3db4154ba874f79b73f7cd9494051c5.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import "./App.css";
-import Books from "./components/BooksContainer";
 
 function App() {
   return (
-    <div className="container">
-      <header className="">
-        <h1 id="app-header">My Reads App </h1>
+    <div className="App">
+      <header className="App-header">
+        <h3>Welcome to the book case app </h3>
       </header>
-      <main>
-        <div className="main">
-          <Books />
-        </div>
-      </main>
-      {/* TODO  Work on the search functionality of the application*/}
     </div>
   );
 }
